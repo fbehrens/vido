ff9ea34b70c17bb6277e67af1b1371878ff7f171
Frank Behrens <fbehrens@gmail.com>
2024-06-28 18:05:37 +0200

    5 examples

---
+++ src/routes/5/1/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { createCounter } from "./counter.svelte.js";
+  const counter = createCounter();
+</script>
+
+<button on:click={counter.increment}>
+  clicks: {counter.count}
+</button>
---
+++ src/routes/5/2/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  let width = $state(1);
+  let height = $state(2);
+
+  let area = $derived(width * height);
+
+  $effect(() => {
+    console.log(area);
+  });
+</script>
+
+<p>
+  <button onclick={() => (width = width + 1)}> width={width} </button>
+</p>
+<p>
+  <button onclick={() => (height = height + 1)}> height={height} </button>
+</p>
+<p>
+  area={area}
+</p>
