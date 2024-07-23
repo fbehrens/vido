2ac807b0f4dd07d89ca62be4cdfb834266056c9d
Frank Behrens <fbehrens@gmail.com>
2024-07-23 17:50:52 +0200

    also delete words

--- src/routes/api/deleteSegment/+server.ts
+++ src/routes/api/deleteSegment/+server.ts
@@ -2,6 +2,11 @@ import { json } from "@sveltejs/kit";
 import { db } from "$lib/db";
 export async function POST({ request, cookies }) {
   const segment = await request.json();
-  const dbResult = db.prepare("delete from segments where id=@id").run(segment);
-  return json({ deletions: dbResult.changes });
+  const deletions = {
+    segments: db.prepare("delete from segments where id=@id").run(segment)
+      .changes,
+    words: db.prepare("delete from words where segment_id=@id").run(segment)
+      .changes,
+  };
+  return json({ deletions });
 }
