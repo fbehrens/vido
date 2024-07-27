fd59f6707d0bed5977376586f42ac9bf840a1c04
Frank Behrens <fbehrens@gmail.com>
2024-07-27 21:59:13 +0200

    YouTube in runes

--- src/lib/components/YouTube.svelte
+++ src/lib/components/YouTube.svelte
@@ -1,22 +1,24 @@
 <script>
   //https://stackoverflow.com/questions/76690112/how-to-use-youtube-embed-api-in-svelte
-  import { onMount } from "svelte";
 
-  export let player;
-  export let initialVideoId = "";
+  let {
+    player = $bindable(),
+    initialVideoId,
+    height = "390",
+    width = "640",
+  } = $props();
 
   const ytPlayerId = "youtube-player";
+  function load() {
+    player = new YT.Player(ytPlayerId, {
+      height, // "100%",
+      width, //"100%",
+      videoId: initialVideoId,
+      playerVars: { autoplay: 1 },
+    });
+  }
 
-  onMount(() => {
-    function load() {
-      player = new YT.Player(ytPlayerId, {
-        height: "390", // "100%",
-        width: "640", //"100%",
-        videoId: initialVideoId,
-        playerVars: { autoplay: 1 },
-      });
-    }
-
+  $effect(() => {
     if (window.YT) {
       load();
     } else {
