4a6f4b73ab0482705e6dc570e4c1d969e5c218d5
Frank Behrens <fbehrens@gmail.com>
2024-07-27 21:45:26 +0200

    simple youtube player

---
+++ src/routes/mockup/youtube/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import YouTube from "$lib/components/YouTube.svelte";
+  let player;
+</script>
+
+<YouTube bind:player initialVideoId="dQw4w9WgXcQ" />
+<button on:click={() => player.seekTo(100, true)}>100</button>
