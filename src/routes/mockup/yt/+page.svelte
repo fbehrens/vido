f89da6e5ea4222a69cbd14f9618d8e97caf778c5
Frank Behrens <fbehrens@gmail.com>
2024-07-27 21:03:56 +0200

    svelte-youtube

---
+++ src/routes/mockup/yt/+page.svelte
@@ -0,0 +1,31 @@
+<svelte:options runes={true} />
+
+<script>
+  import YouTube from "svelte-youtube";
+
+  const options = {
+    height: "390",
+    width: "640",
+    //  see https://developers.google.com/youtube/player_parameters
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  function onReady(event) {
+    // access to player in all event handlers via event.target
+    // event.target.pauseVideo();
+    yt = event.detail.target;
+  }
+  let yt;
+</script>
+
+<button
+  onclick={() => {
+    console.log(yt);
+    yt.seekTo(100, true);
+  }}
+>
+  100</button
+>
+<YouTube videoId="OXGznpKZ_sA" {options} on:ready={onReady} />
