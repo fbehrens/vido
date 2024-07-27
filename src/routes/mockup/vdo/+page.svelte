92d75bec189948d7acefb562ad19b9313fd16c76
Frank Behrens <fbehrens@gmail.com>
2024-07-27 14:47:51 +0200

    simple vdo

---
+++ src/routes/mockup/vdo/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let paused = $state(true);
+  let video;
+  function toggle() {
+    console.log(video);
+    if (paused) video.play();
+    else video.pause();
+  }
+</script>
+
+paused={paused}
+<!-- <button onclick={() => video.play()}>play</button>
+<button onclick={() => video.pause()}>pause</button> -->
+<button onclick={toggle}>toggle</button>
+
+<div>
+  <video
+    bind:this={video}
+    poster="https://sveltejs.github.io/assets/caminandes-llamigos.jpg"
+    src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4"
+    bind:paused
+    onclick={toggle}
+    muted={true}
+  >
+    <track kind="captions" />
+  </video>
+</div>
