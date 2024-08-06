6d62d6f49133702856275430c8c73b5096b61e1b
Frank Behrens <fbehrens@gmail.com>
2024-08-06 17:48:19 +0200

    fix ffmpeg and ffprobe with space in filename

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -3,7 +3,7 @@ import { makeDirFor } from "$lib/util";
 import { exec } from "./util/util";
 
 export async function getDuration(videoPath: string): Promise<number> {
-  let command = `ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ${videoPath}`;
+  let command = `ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${videoPath}"`;
   //   command = `mediainfo --Output='General;%Duration%' ${videoPath}`;
   const info = await exec(command);
   return Number(info.out);
@@ -16,7 +16,7 @@ export async function extractMp3(
   out: string,
 ) {
   makeDirFor(out);
-  const command = `ffmpeg -ss ${start} -to ${end} -i ${filename} -y -acodec libmp3lame -vn ${out}`;
+  const command = `ffmpeg -ss ${start} -to ${end} -i "${filename}" -y -acodec libmp3lame -vn "${out}"`;
   console.log(command);
   const info = await exec(command);
 }
