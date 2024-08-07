1bf06fa022175313a00b8fc4592de44e0e718b14
Frank Behrens <fbehrens@gmail.com>
2024-08-07 15:53:50 +0200

    sse example

---
+++ src/routes/mockup/sse/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+
+  /** @type {string} */
+  let server_time;
+
+  function subscribe() {
+    const sse = new EventSource("/mockup/sse");
+    sse.onmessage = (e) => (server_time = e.data);
+    return () => sse.close();
+  }
+
+  onMount(subscribe);
+</script>
+
+<pre>{server_time}</pre>
