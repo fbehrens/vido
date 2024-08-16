c00bde8d69280a7c4498db4a8fdd5b2e243570b0
Frank Behrens <fbehrens@gmail.com>
2024-08-16 07:53:13 +0200

    show vtt subtitles

---
+++ src/routes/mockup/vtt/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let src = "/mov/zoom/24-07-03";
+</script>
+
+<video width="640" height="360" controls>
+  <source src="{src}.mp4" type="video/mp4" />
+  <track src="{src}.vtt" kind="subtitles" srclang="de" label="Deutsch" />
+</video>
