1bff2079dd17296f7521ffe941d5a5a77fb65a8f
Frank Behrens <fbehrens@gmail.com>
2024-08-16 09:21:15 +0200

    vtt cue styling

--- src/routes/mockup/vtt/+page.svelte
+++ src/routes/mockup/vtt/+page.svelte
@@ -4,5 +4,23 @@
 
 <video width="640" height="360" controls>
   <source src="{src}.mp4" type="video/mp4" />
+  <track
+    src="{src}.test.vtt"
+    kind="captions"
+    srclang="test"
+    label="testlabel"
+  />
   <track src="{src}.vtt" kind="subtitles" srclang="de" label="Deutsch" />
 </video>
+
+<style>
+  ::cue(v[voice="Test"]) {
+    color: #fff;
+    background: #0095dd;
+    outline: solid;
+  }
+  ::cue(v[voice="Frank"]) {
+    color: #0f0;
+    background: #000;
+  }
+</style>
