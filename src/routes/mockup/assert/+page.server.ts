34200e03b36dcaeda8bc7c4c72ff96bfecf4246f
Frank Behrens <fbehrens@gmail.com>
2024-08-23 08:35:38 +0200

    delete mockup

--- src/routes/mockup/assert/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "$lib/db";
-export function load({ params }) {
-  const texts = db.prepare("SELECT text FROM segments").all() as {
-    text: string;
-  }[];
-  return {
-    texts: texts.map((row) => row.text),
-  };
-}
--- src/routes/mockup/assert/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  let { data } = $props();
-  let { texts } = data;
-</script>
-
-{#each texts as text}
-  {text}<br />
-{/each}
