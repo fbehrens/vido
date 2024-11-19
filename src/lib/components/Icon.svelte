270d6a7e90da4e7a67fc4e97e199bcc29338effb
Frank Behrens <fbehrens@gmail.com>
2024-11-19 14:31:05 +0100

    migtrate Icon

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  export let name: "delete" | "debug";
-  const iconData: Record<typeof name, [string, string]> = {
+  const iconData = {
     delete: [
       "0 0 256 256",
       "M216 48h-40v-8a24 24 0 0 0-24-24h-48a24 24 0 0 0-24 24v8H40a8 8 0 0 0 0 16h8v144a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16V64h8a8 8 0 0 0 0-16M96 40a8 8 0 0 1 8-8h48a8 8 0 0 1 8 8v8H96Zm96 168H64V64h128Zm-80-104v64a8 8 0 0 1-16 0v-64a8 8 0 0 1 16 0m48 0v64a8 8 0 0 1-16 0v-64a8 8 0 0 1 16 0",
@@ -9,7 +8,18 @@
       "0 0 32 32",
       "m29.83 20l.34-2l-5.17-.85v-4.38l5.06-1.36l-.51-1.93l-4.83 1.29A9 9 0 0 0 20 5V2h-2v2.23a8.8 8.8 0 0 0-4 0V2h-2v3a9 9 0 0 0-4.71 5.82L2.46 9.48L2 11.41l5 1.36v4.38L1.84 18l.32 2L7 19.18a8.9 8.9 0 0 0 .82 3.57l-4.53 4.54l1.42 1.42l4.19-4.2a9 9 0 0 0 14.2 0l4.19 4.2l1.42-1.42l-4.54-4.54a8.9 8.9 0 0 0 .83-3.57ZM15 25.92A7 7 0 0 1 9 19v-6h6ZM9.29 11a7 7 0 0 1 13.42 0ZM23 19a7 7 0 0 1-6 6.92V13h6Z",
     ],
+    create: ["0 0 24 24", "M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"],
+    transcribe: [
+      "0 0 24 24",
+      "M20 5a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V7c0-1.11.89-2 2-2zm-2 12v-2h-5.5l-2 2zM6 17h2.5l6.85-6.88c.2-.19.2-.51 0-.71l-1.76-1.76c-.2-.2-.52-.2-.71 0L6 14.53z",
+    ],
   };
+  type Name = keyof typeof iconData;
+  interface Props {
+    name: Name;
+    [key: string]: any;
+  }
+  let { name, ...props }: Props = $props();
   const [viewBox, d] = iconData[name];
 </script>
 
@@ -18,5 +28,5 @@
   width="1em"
   height="1em"
   {viewBox}
-  {...$$props}><path fill="currentColor" {d} /></svg
+  {...props}><path fill="currentColor" {d} /></svg
 >
