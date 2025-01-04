ed2870e57e5b0357ae54ee0caaa72b45f108e517
Frank Behrens <fbehrens@gmail.com>
2025-01-04 13:15:55 +0100

    add filtering

--- src/lib/components/ui/data-table.svelte
+++ src/lib/components/ui/data-table.svelte
@@ -1,26 +1,36 @@
 <script lang="ts" generics="TData, TValue">
- import { type ColumnDef, type SortingState, getCoreRowModel, getSortedRowModel } from "@tanstack/table-core";
- import {
-  createSvelteTable,
-  FlexRender,
- } from "$lib/components/ui/data-table/index.js";
- import * as Table from "$lib/components/ui/table/index.js";
+  import {
+    type ColumnDef,
+    type SortingState,
+    type ColumnFiltersState,
+    getCoreRowModel,
+    getSortedRowModel,
+    getFilteredRowModel,
+  } from "@tanstack/table-core";
+  import {
+    createSvelteTable,
+    FlexRender,
+  } from "$lib/components/ui/data-table/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import * as Table from "$lib/components/ui/table/index.js";
 
- type DataTableProps<TData, TValue> = {
-  columns: ColumnDef<TData, TValue>[];
-  data: TData[];
- };
+  type DataTableProps<TData, TValue> = {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+  };
 
- let { data, columns }: DataTableProps<TData, TValue> = $props();
- let sorting = $state<SortingState>([]);
+  let { data, columns }: DataTableProps<TData, TValue> = $props();
+  let sorting = $state<SortingState>([]);
+  let columnFilters = $state<ColumnFiltersState>([]);
 
- const table = createSvelteTable({
-  get data() {
-   return data;
-  },
-  columns,
-  getCoreRowModel: getCoreRowModel(),
-  getSortedRowModel: getSortedRowModel(),
+  const table = createSvelteTable({
+    get data() {
+      return data;
+    },
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     onSortingChange: (updater) => {
       if (typeof updater === "function") {
         sorting = updater(sorting);
@@ -28,51 +38,76 @@
         sorting = updater;
       }
     },
+    onColumnFiltersChange: (updater) => {
+      if (typeof updater === "function") {
+        columnFilters = updater(columnFilters);
+      } else {
+        columnFilters = updater;
+      }
+    },
     state: {
       get sorting() {
         return sorting;
       },
+      get columnFilters() {
+        return columnFilters;
+      },
     },
- });
+  });
 </script>
 
-<div class="rounded-md border">
- <Table.Root>
-  <Table.Header>
-   {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
-    <Table.Row>
-     {#each headerGroup.headers as header (header.id)}
-      <Table.Head>
-       {#if !header.isPlaceholder}
-        <FlexRender
-         content={header.column.columnDef.header}
-         context={header.getContext()}
-        />
-       {/if}
-      </Table.Head>
-     {/each}
-    </Table.Row>
-   {/each}
-  </Table.Header>
-  <Table.Body>
-   {#each table.getRowModel().rows as row (row.id)}
-    <Table.Row data-state={row.getIsSelected() && "selected"}>
-     {#each row.getVisibleCells() as cell (cell.id)}
-      <Table.Cell>
-       <FlexRender
-        content={cell.column.columnDef.cell}
-        context={cell.getContext()}
-       />
-      </Table.Cell>
-     {/each}
-    </Table.Row>
-   {:else}
-    <Table.Row>
-     <Table.Cell colspan={columns.length} class="h-24 text-center">
-      No results.
-     </Table.Cell>
-    </Table.Row>
-   {/each}
-  </Table.Body>
- </Table.Root>
+<div>
+  <div class="flex items-center py-4">
+    <Input
+      placeholder="Filter emails..."
+      value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+      onchange={(e) => {
+        table.getColumn("email")?.setFilterValue(e.currentTarget.value);
+      }}
+      oninput={(e) => {
+        table.getColumn("email")?.setFilterValue(e.currentTarget.value);
+      }}
+      class="max-w-sm"
+    />
+  </div>
+  <div class="rounded-md border">
+    <Table.Root>
+      <Table.Header>
+        {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+          <Table.Row>
+            {#each headerGroup.headers as header (header.id)}
+              <Table.Head>
+                {#if !header.isPlaceholder}
+                  <FlexRender
+                    content={header.column.columnDef.header}
+                    context={header.getContext()}
+                  />
+                {/if}
+              </Table.Head>
+            {/each}
+          </Table.Row>
+        {/each}
+      </Table.Header>
+      <Table.Body>
+        {#each table.getRowModel().rows as row (row.id)}
+          <Table.Row data-state={row.getIsSelected() && "selected"}>
+            {#each row.getVisibleCells() as cell (cell.id)}
+              <Table.Cell>
+                <FlexRender
+                  content={cell.column.columnDef.cell}
+                  context={cell.getContext()}
+                />
+              </Table.Cell>
+            {/each}
+          </Table.Row>
+        {:else}
+          <Table.Row>
+            <Table.Cell colspan={columns.length} class="h-24 text-center">
+              No results.
+            </Table.Cell>
+          </Table.Row>
+        {/each}
+      </Table.Body>
+    </Table.Root>
+  </div>
 </div>
