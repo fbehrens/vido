490ea6f6e9c3688b53fbcb96ac902ad3e051f9b6
Frank Behrens <fbehrens@gmail.com>
2025-01-04 18:03:56 +0100

    move data-table

--- src/routes/mockup/payments/+page.svelte
+++ src/routes/mockup/payments/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
- import DataTable from "$lib/components/ui/data-table.svelte";
- import { columns } from "./columns";
+  import DataTable from "./data-table.svelte";
+  import { columns } from "./columns";
 
- let { data } = $props();
- let {payments} = data
+  let { data } = $props();
+  let { payments } = data;
 </script>
 
 <DataTable data={payments} {columns} />
