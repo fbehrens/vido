a5a553569bd353448b5d228d7e674d00303b62b9
Frank Behrens <fbehrens@gmail.com>
2025-01-04 12:36:04 +0100

    basic table

---
+++ src/routes/mockup/payments/+page.server.ts
@@ -0,0 +1,8 @@
+import { payments } from "./columns";
+
+export async function load() {
+  // logic to fetch payments data here
+  return {
+    payments,
+  };
+}
---
+++ src/routes/mockup/payments/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+ import DataTable from "$lib/components/ui/data-table.svelte";
+ import { columns } from "./columns";
+
+ let { data } = $props();
+ let {payments} = data
+</script>
+
+<DataTable data={payments} {columns} />
