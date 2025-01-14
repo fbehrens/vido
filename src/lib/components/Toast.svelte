c994d71488578cf0d80c08facffbca53cdce3588
Frank Behrens <fbehrens@gmail.com>
2025-01-14 13:13:17 +0100

    remove Toast on click

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import { getToastState } from "$lib/toast-state.svelte";
   import type { Toast } from "$lib/types";
   import { CircleX } from "lucide-svelte/icons";
   type Props = { toast: Toast };
   let { toast }: Props = $props();
+  const toastState = getToastState();
 </script>
 
 <div
@@ -10,7 +12,12 @@
 >
   <span class="text-sm font-medium">{toast.title}</span>
   <span class="text-xs">{toast.message}</span>
-  <button class="absolute right-2 top-2 size-5">
+  <button
+    class="absolute right-2 top-2 size-5"
+    onclick={() => {
+      toastState.remove(toast.id);
+    }}
+  >
     <span class="sr-only">Close toast</span>
     <CircleX />
   </button>
