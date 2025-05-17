f01cf546aa5e0cabde7620f98c67fdcd783d5830
Frank Behrens <fbehrens@gmail.com>
2025-05-17 12:06:23 +0200

    add youtube transcript

---
+++ packages/youtube-transcript/test/index.test.ts
@@ -0,0 +1,20 @@
+import { assertEquals } from "jsr:@std/assert";
+import { YoutubeTranscript } from "../src/index.ts";
+
+const videoId = "dQw4w9WgXcQ";
+Deno.test("fetchTranscript", async () => {
+  const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+  assertEquals(transcript[1], {
+    duration: 4.32,
+    lang: "de-DE",
+    offset: 22.64,
+    text: "Du kennst die Regeln genau wie ich",
+  });
+});
+
+Deno.test("textOnly", async () => {
+  const transcript = await YoutubeTranscript.fetchTranscript(videoId, {
+    textOnly: true,
+  });
+  assertEquals(transcript.slice(0, 20), " Uns beiden ist die ");
+});
