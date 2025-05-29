f6c17a66f23b8d25af795227831562828e51ad78
Frank Behrens <fbehrens@gmail.com>
2025-05-29 10:14:30 +0200

    summariseYoutube

--- packages/cmd/index.ts
+++ packages/cmd/index.ts
@@ -1,11 +1,27 @@
 import { generateText } from "ai";
 import { openai } from "@ai-sdk/openai";
+import { YoutubeTranscript } from "youtube-transcript";
 
-export async function generateOpenai(prompt: string) {
+const model = openai("o3-mini");
+export async function chatCompletion(prompt: string) {
   return await generateText({
-    model: openai("o3-mini"),
+    model,
     prompt,
   });
+}
+
+export async function summariseYoutube(videoId: string) {
+  const transcript = await YoutubeTranscript.fetchTranscript(videoId, {
+    textOnly: true,
+  });
+  //   console.log("Transcript fetched:", transcript);
+  const prompt = `\
+Summarise the following transcript from a Youtube video.
+Please use markdown as output format.
+
+${transcript}`;
+  const completion = await chatCompletion(prompt);
+  return completion.text;
 }
 
 export function add(a: number, b: number) {
@@ -14,5 +30,5 @@ export function add(a: number, b: number) {
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Generated text:", generateOpenai("hi"));
+  console.log("Generated text:", chatCompletion("hi"));
 }
--- packages/cmd/index_test.ts
+++ packages/cmd/index_test.ts
@@ -1,11 +1,17 @@
 import { assertEquals, assert } from "@std/assert";
-import { add, generateOpenai } from "./index.ts";
+import { add, chatCompletion, summariseYoutube } from "./index.ts";
 
 Deno.test("addTest", () => {
   assertEquals(add(2, 3), 5);
 });
 
 Deno.test("generateOpenaiTest", async () => {
-  const r = await generateOpenai("hi");
+  const r = await chatCompletion("hi");
   assert(typeof r.text === "string");
+});
+Deno.test("summariseYoutube", async () => {
+  const videoId = "dQw4w9WgXcQ";
+  const r = await summariseYoutube(videoId);
+  console.log({ r });
+  assert(typeof r === "string");
 });
