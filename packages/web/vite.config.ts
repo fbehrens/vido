a121fd0a9cfea7411c0e240947a715339aa6f983
Frank Behrens <fbehrens@gmail.com>
2025-06-29 22:27:53 +0200

    tailscale tunnel

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,6 +2,10 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: "127.0.0.1",
+    allowedHosts: ["honolulu.kite-ling.ts.net"],
+  },
   plugins: [sveltekit()],
   resolve: {
     alias: {
