68168614b612d8fe07303b4a01d5e3ad336e3aaf
Frank Behrens <fbehrens@gmail.com>
2025-08-02 20:45:02 +0200

    async and remote functions

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -12,12 +12,20 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    experimental: {
+      remoteFunctions: true,
+    },
   },
   vitePlugin: {
     inspector: {
       toggleKeyCombo: "meta-shift",
       showToggleButton: "always",
       toggleButtonPos: "bottom-right",
+    },
+  },
+  compilerOptions: {
+    experimental: {
+      async: true,
     },
   },
 };
