a20aece53d00e1ad08c93924fe64abad07f29c71
Frank Behrens <fbehrens@gmail.com>
2025-08-02 14:23:50 +0200

    remove toas

--- packages/web/src/routes/mockup/+layout.svelte
+++ packages/web/src/routes/mockup/+layout.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
-  import { page } from "$app/state";
-  import { setToastState } from "$lib/toast-state.svelte.js";
-  import { cn } from "$lib/utils";
-
-  let { children, data } = $props();
-  const { pages } = data;
-  setToastState();
+	import { page } from '$app/state';
+	import { cn } from '$lib/utils';
+	let { children, data } = $props();
+	const { pages } = data;
 </script>
 
-<div class="flex items-center gap-2 text-sm border-[1px] border-grey">
-  {#each pages as p}
-    <a
-      class={cn(
-        "transition-colors hover:text-foreground/80",
-        `/mockup/${p}` == page.route.id
-          ? "text-foreground"
-          : "text-foreground/60",
-      )}
-      href={`/mockup/${p}`}>{p}</a
-    >
-  {/each}
+<div class="border-grey flex items-center gap-2 border-[1px] text-sm">
+	{#each pages as p}
+		<a
+			class={cn(
+				'hover:text-foreground/80 transition-colors',
+				`/mockup/${p}` == page.route.id ? 'text-foreground' : 'text-foreground/60'
+			)}
+			href={`/mockup/${p}`}>{p}</a
+		>
+	{/each}
 </div>
 {@render children()}
