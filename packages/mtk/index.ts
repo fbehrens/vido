8d9c4e88b22e63e622f7e4105d37dc3d13e3ecf7
Frank Behrens <fbehrens@gmail.com>
2025-08-09 17:09:35 +0200

    use Effect

--- packages/mtk/index.ts
+++ packages/mtk/index.ts
@@ -1,12 +1,22 @@
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
+import { Effect, Config, Layer } from "effect";
 import { person } from "./drizzle/schema";
 
-const db = new Database("local.db");
+import * as SqliteDrizzle from "@effect/sql-drizzle/Sqlite";
+import { SqliteClient } from "@effect/sql-sqlite-bun";
 
-const drz = drizzle({ client: db });
+const SqlLive = SqliteClient.layer({
+  filename: "local.db",
+});
 
-// const row = db.query("SELECT * FROM person;").get();
+const DrizzleLive = SqliteDrizzle.layer.pipe(Layer.provide(SqlLive));
 
-const row = await drz.select().from(person);
-console.log(row);
+export const DatabaseLive = Layer.mergeAll(SqlLive, DrizzleLive);
+
+const program = Effect.gen(function* (_) {
+  const db = yield* SqliteDrizzle.SqliteDrizzle;
+  const row = yield* db.select().from(person);
+  console.log(row);
+});
+
+const runnable = Effect.provide(program, DatabaseLive);
+Effect.runPromise(runnable).catch(console.error);
