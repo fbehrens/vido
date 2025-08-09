64182556d22a320d8c73513bd4b933ec8ee749a8
Frank Behrens <fbehrens@gmail.com>
2025-08-09 10:29:18 +0200

    add view

--- packages/mtk/drizzle/schema.ts
+++ packages/mtk/drizzle/schema.ts
@@ -1,7 +1,17 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import {
+  sqliteTable,
+  text,
+  integer,
+  sqliteView,
+} from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm";
 
 export const person = sqliteTable("person", {
   name: text(),
   age: integer(),
 });
+
+export const person18 = sqliteView("person_18", {
+  name: text(),
+  age: integer(),
+}).as(sql`select * from person where age > 18`);
