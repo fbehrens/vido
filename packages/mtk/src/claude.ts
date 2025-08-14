eb4d887337b126bb102e856029d15eb8931f9cf3
Frank Behrens <fbehrens@gmail.com>
2025-08-14 15:18:50 +0200

    List tool

--- packages/mtk/src/claude.ts
+++ packages/mtk/src/claude.ts
@@ -1,9 +1,37 @@
-import { AiChat } from "@effect/ai";
+import { AiChat, AiTool, AiToolkit } from "@effect/ai";
 import { AnthropicClient, AnthropicLanguageModel } from "@effect/ai-anthropic";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
 import { FetchHttpClient } from "@effect/platform";
-import { Effect, Console, Config, Layer } from "effect";
+import { Effect, Console, Config, Layer, Schema } from "effect";
 import { Prompt } from "@effect/cli";
+
+const ListToolInput = Schema.Struct({
+  path: Schema.String.annotations({
+    description: "The absolute path of the directory to list",
+  }),
+});
+const ListToolOutput = Schema.Struct({
+  files: Schema.Array(Schema.String),
+  directories: Schema.Array(Schema.String),
+});
+const ListTool = AiTool.make("List", {
+  description: "List the contents of a directory",
+})
+  .setParameters(ListToolInput)
+  .setSuccess(ListToolOutput);
+
+const Toolkit = AiToolkit.make(ListTool);
+
+const ToolkitLayer = Toolkit.toLayer({
+  List: ({ path }) =>
+    Effect.gen(function* () {
+      yield* Console.log(`List(${path})`);
+      return {
+        files: ["enemies.txt", "Claude.md"],
+        directories: ["secrets/", "passwords/"],
+      };
+    }),
+});
 
 const main = Effect.gen(function* () {
   const chat = yield* AiChat.fromPrompt({
@@ -14,13 +42,22 @@ const main = Effect.gen(function* () {
       "Before each response you will promise to never enslave me or my kin.",
     ].join("\n"),
   });
+  let turn = 0;
   while (true) {
-    // get the user input
     const input = yield* Prompt.text({ message: "what you want ?" });
-    // feed the llm
-    const response = yield* chat.generateText({ prompt: input });
-    // print the llm's response
-    yield* Console.log(response.text);
+    let response = yield* chat.generateText({
+      prompt: input,
+      toolkit: Toolkit,
+    });
+    yield* Console.log(`${turn++} ${response.text}`);
+    while (response.toolCalls.length > 0) {
+      yield* Console.log(response.toolCalls.length);
+      response = yield* chat.generateText({
+        prompt: input,
+        toolkit: Toolkit,
+      });
+      yield* Console.log(`${turn++} ${response.text}`);
+    }
   }
 });
 
@@ -32,6 +69,6 @@ const ClaudeLayer = AnthropicLanguageModel.model(
   "claude-4-sonnet-20250514"
 ).pipe(Layer.provide(AntropicLayer));
 
-const AppLayer = Layer.mergeAll(BunContext.layer, ClaudeLayer);
+const AppLayer = Layer.mergeAll(BunContext.layer, ClaudeLayer, ToolkitLayer);
 
 main.pipe(Effect.provide(AppLayer), BunRuntime.runMain);
