a65468f005e4508859e941868be834f1982e77d4
Frank Behrens <fbehrens@gmail.com>
2025-08-20 07:54:05 +0200

    effect schema tests

--- packages/web/src/lang/schema.test.ts
+++ packages/web/src/lang/schema.test.ts
@@ -1,19 +1,49 @@
-import { describe, test, expect } from "vitest";
-import { Schema } from "effect";
+import { it, expect } from "@effect/vitest";
+import { Schema, Effect } from "effect";
 
+it.effect("effectful", () => Effect.sync(() => expect(1).toBe(1)));
+
+const source = Schema.Literal("on", "off");
+const target = Schema.Boolean;
+// <bool,on|off,never
 const BooleanFromString = Schema.transform(
-  Schema.Literal("on", "off"), // Source schema: "on" or "off"
-  Schema.Boolean, // Target schema: boolean
+  source,
+  target, // Target schema: boolean
   {
     strict: true,
     decode: (literal) => literal === "on", // Always succeeds here
     encode: (bool) => (bool ? "on" : "off"),
   },
 );
-type Encoded = typeof BooleanFromString.Encoded;
 type Type = typeof BooleanFromString.Type;
-describe("schema", () => {
-  test("", () => {
-    expect(Schema.decodeUnknownSync(BooleanFromString)("on")).toBe(true);
-  });
+type Encoded = typeof BooleanFromString.Encoded;
+
+it("transform", () => expect(Schema.decodeUnknownSync(BooleanFromString)("on")).toBe(true));
+
+it.effect("effect", () => Effect.sync(() => expect(1).toEqual(1)));
+
+const Video = Schema.Struct({
+  title: Schema.String,
 });
+const VideoData = Schema.parseJson(Video);
+
+const encoded = `{"title":"Test Video"}`;
+const decoded = { title: "Test Video" };
+const decoded_ = { title_: "Test Video" };
+
+it("VideoData", () => {
+  expect(Schema.encodeUnknownSync(VideoData)(decoded)).toEqual(encoded);
+  expect(Schema.decodeSync(VideoData)(encoded)).toStrictEqual(decoded);
+});
+
+const Video_ = Schema.Struct({
+  title_: Schema.String,
+});
+
+const VideoData_ = Schema.transform(VideoData, Video_, {
+  strict: true,
+  decode: (data) => ({ title_: data.title }),
+  encode: (video) => ({ title: video.title_ }),
+});
+
+it("VideoData_", () => expect(Schema.decodeSync(VideoData_)(encoded)).toStrictEqual(decoded_));
---
+++ packages/web/src/lang/vitest.test.ts
@@ -0,0 +1,2 @@
+import { describe, it, expect } from "vitest";
+it("works", () => expect(1).toEqual(1));
