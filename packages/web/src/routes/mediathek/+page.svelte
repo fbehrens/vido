98413e7ef9f0b3ea45ae3c325f6000e19f5eb2a9
Frank Behrens <fbehrens@gmail.com>
2025-08-28 12:17:16 +0200

    table on the page

--- packages/web/src/routes/mediathek/+page.svelte
+++ packages/web/src/routes/mediathek/+page.svelte
@@ -1,13 +1,69 @@
 <script lang="ts">
+  import {
+    type ColumnFiltersState,
+    type VisibilityState,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getSortedRowModel,
+    type RowSelectionState,
+  } from "@tanstack/table-core";
+  import { createSvelteTable, FlexRender } from "$lib/components/ui/data-table/index.js";
+  import * as Table from "$lib/components/ui/table/index.js";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
   import { Button } from "$lib/components/ui/button";
-  import Input from "$lib/components/ui/input/input.svelte";
   import { columns } from "./columns";
-  import DataTable from "./data-table.svelte";
   import { getFlms } from "./data.remote";
+  import { Input } from "$lib/components/ui/input";
   let search = $state("lanz");
   let limit = $state("1000");
   const param = $derived({ search, limit: Number(limit) });
   const { data, count } = $derived(await getFlms(param));
+
+  let columnFilters = $state<ColumnFiltersState>([]);
+  let rowSelection = $state<RowSelectionState>({});
+  let columnVisibility = $state<VisibilityState>({ beschreibung: false });
+
+  const table = createSvelteTable({
+    get data() {
+      return data;
+    },
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: (updater) => {
+      if (typeof updater === "function") {
+        columnFilters = updater(columnFilters);
+      } else {
+        columnFilters = updater;
+      }
+    },
+    onRowSelectionChange: (updater) => {
+      if (typeof updater === "function") {
+        rowSelection = updater(rowSelection);
+      } else {
+        rowSelection = updater;
+      }
+    },
+    onColumnVisibilityChange: (updater) => {
+      if (typeof updater === "function") {
+        columnVisibility = updater(columnVisibility);
+      } else {
+        columnVisibility = updater;
+      }
+    },
+    state: {
+      get columnFilters() {
+        return columnFilters;
+      },
+      get rowSelection() {
+        return rowSelection;
+      },
+      get columnVisibility() {
+        return columnVisibility;
+      },
+    },
+  });
 </script>
 
 <div class="flex flex-row">
@@ -18,6 +74,65 @@
     <span class="text-sm">/{count})</span>
   </div>
   <Button onclick={() => getFlms(param).refresh()} variant="outline">refresh</Button>
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger>
+      {#snippet child({ props })}
+        <Button {...props} variant="outline" class="ml-auto">Columns</Button>
+      {/snippet}
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content align="end">
+      {#each table.getAllColumns().filter((col) => col.getCanHide()) as column (column.id)}
+        <DropdownMenu.CheckboxItem
+          class="capitalize"
+          bind:checked={() => column.getIsVisible(), (v) => column.toggleVisibility(!!v)}
+        >
+          {column.id}
+        </DropdownMenu.CheckboxItem>
+      {/each}
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
 </div>
 
-<DataTable {data} {columns} />
+<div class="space-y-4">
+  <div class="rounded-md border">
+    <Table.Root>
+      <Table.Header>
+        {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+          <Table.Row>
+            {#each headerGroup.headers as header (header.id)}
+              <Table.Head colspan={header.colSpan}>
+                {#if !header.isPlaceholder}
+                  <FlexRender
+                    content={header.column.columnDef.header!}
+                    context={header.getContext()}
+                  />
+                {/if}
+              </Table.Head>
+            {/each}
+          </Table.Row>
+        {/each}
+      </Table.Header>
+      <Table.Body>
+        {#each table.getRowModel().rows as row (row.id)}
+          <Table.Row data-state={row.getIsSelected() && "selected"}>
+            {#each row.getVisibleCells() as cell (cell.id)}
+              <Table.Cell>
+                <FlexRender content={cell.column.columnDef.cell!} context={cell.getContext()} />
+              </Table.Cell>
+            {/each}
+          </Table.Row>
+        {:else}
+          <Table.Row>
+            <Table.Cell colspan={columns.length} class="h-24 text-center">No results.</Table.Cell>
+          </Table.Row>
+        {/each}
+      </Table.Body>
+    </Table.Root>
+  </div>
+  <div class="flex items-center justify-end space-x-2 py-4">
+    <div class="flex-1 text-sm text-muted-foreground">
+      {table.getFilteredSelectedRowModel().rows.length} of{" "}
+      {table.getFilteredRowModel().rows.length} row(s) selected.
+    </div>
+  </div>
+</div>
