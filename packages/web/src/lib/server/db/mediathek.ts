3b23fe45ef674260d1989ee04654f5cb83efe6de
Frank Behrens <fbehrens@gmail.com>
2025-11-12 22:24:20 +0100

    remove unused files

--- packages/web/src/lib/server/db/mediathek.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "$env/dynamic/private";
-import Database from "better-sqlite3";
-import * as schema_mediathek from "./schema/mediathek";
-
-import { drizzle } from "drizzle-orm/better-sqlite3";
-if (!env.MEDIATHEK_URL) throw new Error("MEDIATHEK_URL is not set");
-export const dbPathMediathek = env.MEDIATHEK_URL;
-export const db_mediathek_better = new Database(dbPathMediathek);
-export const db_mediathek = drizzle({ client: db_mediathek_better, schema: schema_mediathek });
